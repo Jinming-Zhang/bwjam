26251b49ace35281722fde6faa19e5ed91313cf8
Jinming-Zhang <jinming.zhang47@gmail.com>
2022-04-11 02:16:37 -0400

    fix grandma enemy

---
+++ Assets/GamePlay/Scripts/GrandmaEnemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandmaEnemy : Enemy
+{
+    [SerializeField]
+    GameObject goodGrandma;
+
+    public override void OnDead()
+    {
+        base.OnDead();
+        Animator.Play(AnimationConstants.Spirit_Die);
+    }
+    public override void OnDeadAnimationFinished()
+    {
+        base.OnDeadAnimationFinished();
+        // spawn good grandma
+        Instantiate(goodGrandma, transform.position, Quaternion.identity);
+    }
+
+    public override void DoIdleAnimation()
+    {
+        base.DoIdleAnimation();
+        Animator.Play(AnimationConstants.Spirit_Idle);
+    }
+    public override void DoAttackAnimation()
+    {
+        base.DoAttackAnimation();
+        Animator.Play(AnimationConstants.Spirit_Spit);
+    }
+    public override void DoDamagedAnimation()
+    {
+        base.DoDamagedAnimation();
+        Animator.Play(AnimationConstants.Spirit_Hit);
+    }
+    public override void DoWalkAnimation()
+    {
+        base.DoWalkAnimation();
+        Animator.Play(AnimationConstants.Spirit_Walk);
+    }
+
+}
